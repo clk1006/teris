72999d1734beef7791300cfa45a9bf993c81f01c
tangthetank <97601007+tangthetank@users.noreply.github.com>
2022-01-27 09:10:43 +0100

    Update api.js

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,64 +1,64 @@
-const dbClient=require("./db.js")
-let storage={
-    id:"stoTet",
-    state:0,
-    score:0,
-    tiles:Array(200).fill(0),
-    current:{
-        type:0,
-        pos:0,
-        rot:0
+const dbClient = require("./db.js")
+let storage = {
+    id: "stoTet",
+    state: 0,
+    score: 0,
+    tiles: Array(200).fill(0),
+    current: {
+        type: 0,
+        pos: 0,
+        rot: 0
     },
     next:0
 }
-const getColumns=(curr)=>{
-    switch(curr.type){
+const getColumns = (curr) => {
+    switch(curr.type) {
         case 0:
-            return (rot%2==0?4:1)
+            return (rot % 2 == 0 ? 4:1)
         case 1:
-            return (rot%2==0?3:2)
+            return (rot % 2 == 0 ? 3:2)
         case 2:
-            return (rot%2==0?3:2)
+            return (rot % 2 == 0 ? 3:2)
         case 3:
-            return (rot%2==0?3:2)
+            return (rot % 2 == 0 ? 3:2)
         case 4:
-            return (rot%2==0?3:2)
+            return (rot % 2 == 0 ? 3:2)
         case 5:
-            return (rot%2==0?3:2)
+            return (rot % 2 == 0 ? 3:2)
         case 6:
-            return (rot%2==0?2:2)
+            return (rot % 2 == 0 ? 2:2)
     }
 }
-const getScore=(map,player)=>{
-    let troops=3
-    map.continents.forEach((x)=>{
-        for(let i=0;i<x.tiles.length;i++){
-            if(map.tiles[x.tiles[i]].controller!=player){
+const getScore = (map, player) => {
+    let troops = 3
+    map.continents.forEach((x) => {
+        for(let i = 0; i<x.tiles.length; i++) {
+            if(map.tiles[x.tiles[i]].controller != player) {
                 return
             }
         }
-        troops+=x.reward
+        troops += x.reward
     })
     return troops
 }
-const dropBlock=(block,tiles)=>{
-    let id=tiles.reduce((a,b)=>Math.max(a,b),0)+1
-    let columns=getColumns(block)
-    let row=19
-    let column=pos
-    Array(1).fill(0).forEach((_)=>{
-        for(;row>=0;row++){
-            for(;column<pos+columns;column++){
-                if(tiles[10*row+column]!=0){
+const dropBlock = (block,tiles) => {
+    let id = tiles.reduce((a,b) => Math.max(a, b), 0) + 1
+    let columns = getColumns(block)
+    let row = 19
+    let column = pos
+    Array(1).fill(0).forEach((_) => {
+        for(;row>=0; row++) {
+            for(;column < pos+columns; column++) {
+                if(tiles[10*row+column] != 0) {
                     return
                 }
             }
         }
     })
-    if(!(row==0&&column==columns+pos-1&&tiles[10*row+column]==0)){
+    if(!(row == 0 && column == columns + pos - 1 && tiles[10*row+column] == 0)) {
         row++
     }
-    Array(columns).fill(0).forEach((_,col)=>{
+    Array(columns).fill(0).forEach((_, col) => {
         tiles[10*row+col+pos]=id
     })
 }
