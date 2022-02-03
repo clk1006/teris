bef2d32877b44d1d7f3f944359e8538fac277b13
clk1006 <clk1006@web.de>
2022-02-03 17:42:51 +0100

    rotation should be fixed

--- pages/api/api.js
+++ pages/api/api.js
@@ -16,18 +16,18 @@ let storage = {
 }
 
 const rotateArray = (arr, rot) => {
-    let [cols, rows] = rot % 2 == 0 ? [arr.length, arr[0].length] : [arr[0].length, arr.length]
+    let [rows, cols] = rot % 2 == 0 ? [arr.length, arr[0].length] : [arr[0].length, arr.length]
 
     return Array(rows).fill(0).map((_, row) => Array(cols).fill(0).map((_, col) => {
         switch (rot % 4) {
             case 0:
                 return arr[row][col]
             case 1:
-                return arr[col][arr[0].length - row]
+                return arr[arr.length - col - 1][row]
             case 2:
-                return arr[arr[0].length - row][arr.length - col]
+                return arr[arr.length - row - 1][arr[0].length - col - 1]
             case 3:
-                return arr[arr.length - col][row]
+                return arr[col][arr[0].length - row - 1]
         }
     }))
 }
