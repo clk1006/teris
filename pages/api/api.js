6fc48ea82c92f09bff698c8585d13cbd4817547a
clk1006 <clk1006@web.de>
2022-02-24 17:50:03 +0100

    fixed error with array deconstruction

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,20 +1,21 @@
 const fs=require('fs')
 let data=require('../../data/storage.json')
+const seedrandom=require("seedrandom")
+const { ConnectionPoolClosedEvent } = require('mongodb')
 const SCORE_BLOCK = 5
 const SCORE_CLEAR = 100
-
-let storage = {
+const STORAGE_BASE={
+    id: "stoTet",
     state: 0,
     score: 0,
     tiles: Array(200).fill(0),
     current: {
         type: 0,
-        pos: 0,
+        pos: 4,
         rot: 0
     },
-    next: 0
+    seq:[0,1,2,3,4,5,6]
 }
-
 const rotateArray = (arr, rot) => {
     let [rows, cols] = rot % 2 == 0 ? [arr.length, arr[0].length] : [arr[0].length, arr.length]
 
@@ -31,7 +32,6 @@ const rotateArray = (arr, rot) => {
         }
     }))
 }
-
 const getShape = (block) => {
     let arr = []
 
@@ -81,8 +81,27 @@ const getShape = (block) => {
     return rotateArray(arr, block.rot)
 }
 
+const shuffle=(arr,rng)=>{
+    let currentIndex = arr.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(rng() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [arr[currentIndex], arr[randomIndex]] = [
+        arr[randomIndex], arr[currentIndex]];
+    }
+  
+    return [arr,rng];
+  }
+
 const getOccupiedTiles = (pos, shape) => {
     let tiles = []
+
     shape.forEach((x, row) => x.forEach((val, col) => {
         if (val == 1) {
             tiles.push(10 * (pos.y - row) + pos.x + col)
@@ -92,39 +111,42 @@ const getOccupiedTiles = (pos, shape) => {
     return tiles
 }
 
-const copy = (a) => {
-    return JSON.parse(JSON.stringify(a))
-}
-
 const dropBlock = (block, tiles) => {
     let id = tiles.reduce((a, b) => Math.max(a, b)) + 1
     let shape = getShape(block)
-    for (let i = 0; i < 20; i++) {
+
+    for (let i = 19; i >= 0; i--) {
         let pos = {
             x: block.pos,
             y: i
         }
-        let tilesOcc = getOccupiedTiles(pos, shape)
-        console.log(tilesOcc)
         let fits = true
-        let tilesNew = copy(tiles)
+        let tilesOcc = getOccupiedTiles(pos, shape)
         tilesOcc.forEach((x) => {
             if (tiles[x] != 0) {
                 fits = false
-            } else {
-                tilesNew[x] = id
             }
         })
-
-        if (fits) {
-            return [true, tilesNew]
+        if(fits && i-shape.length>-1){
+            continue
         }
+        if(!fits && i==19){
+            return [false]
+        }
+        if (!fits){
+            pos.y++
+            tilesOcc = getOccupiedTiles(pos, shape)
+        }
+        
+        tilesOcc.forEach((x)=>{
+            tiles[x]=id;
+        })
+        return [true,tiles]
     }
-
-    return [false]
 }
 
 const updateState = (score, tiles) => {
+
     for (let row = 0; row < 20; row++) {
         let full = true
 
@@ -148,15 +170,18 @@ const updateState = (score, tiles) => {
             }
 
             score += SCORE_CLEAR
+            row--
         }
     }
-    
-    return [score, tiles]
+    let state=tiles.filter((_,i)=>i>189).filter((x)=>x!=0).length==0 ? 0 : 1
+    return [score, tiles, state]
 }
 
 module.exports = async (req, res) => {
-    let gameId=req.query.gameId
+    let storage=STORAGE_BASE
+    let gameId=req.query.gameId || 0
     let index=0
+    let rng
     if(data.filter(x=>x.gameId==gameId).length==1){
         storage=data.filter(x=>x.gameId==gameId)[0]
         data.forEach((x,i)=>{
@@ -164,30 +189,51 @@ module.exports = async (req, res) => {
                 index=i
             }
         })
+        rng=seedrandom(storage.seed)
+        Array(storage.seedUsed).fill(0).forEach(()=>{
+            rng()
+        })
     }
     else{
         storage.gameId=gameId
+        storage.seed=req.query.seed || Math.random().toString()
+        rng=seedrandom(storage.seed)
+        storage.seedUsed=2
+        storage.current.type=Math.floor(rng()*7)
+        let shuffled=shuffle(storage.seq,rng)
+        storage.seq=shuffled[0]
+        rng=shuffled[1]
         data.push({})
         index=data.length-1
     }
+
     switch (req.query.type) {
         case "getState":
-            res.status(200).json([storage.score, storage.tiles, storage.current, storage.next])
+            res.status(200).json([storage.score, storage.tiles, storage.current, storage.seq[0], storage.state])
             break
         case "endTurn":
             let dropRes = dropBlock(storage.current, storage.tiles)
             if (!dropRes[0]) {
                 res.status(404).send()
+
                 break
             }
             let state = updateState(storage.score, dropRes[1])
             storage.score=state[0]+SCORE_BLOCK
             storage.tiles=state[1]
-            storage.current.type = storage.next
+            storage.current.type = storage.seq.shift()
             storage.current.pos = 4
             storage.current.rot = 0
-            storage.next = Math.floor(Math.random() * 7)
-            res.status(200).json([storage.score, storage.tiles, storage.current, storage.next])
+            storage.state=state[2]
+
+            if(storage.seq.length==0){
+                let shuffled=shuffle(storage.seq,rng)
+                storage.seq=shuffled[0]
+                rng=shuffled[1]
+                storage.seedUsed++
+            }
+            res.status(200).json([storage.score, storage.tiles, storage.current, storage.seq[0],storage.state])
+            
             break
         case "moveLeft":
             if (storage.current.pos > 0) {
@@ -196,37 +242,62 @@ module.exports = async (req, res) => {
             } else {
                 res.status(404).send()
             }
+
             break
         case "moveRight":
             let shape = getShape(storage.current)
+
             if (storage.current.pos + shape[0].length < 10) {
                 storage.current.pos++
                 res.status(200).json(storage.current)
             } else {
                 res.status(404).send()
             }
+
             break
         case "rotLeft": 
             storage.current.rot = (storage.current.rot - 1) % 4
             shape = getShape(storage.current)
+
             while (storage.current.pos + shape[0].length > 10) {
                 storage.current.pos--
             }
+
             res.status(200).json(storage.current)
+
             break
         case "rotRight": 
             storage.current.rot = (storage.current.rot + 1) % 4
             shape = getShape(storage.current)
+
             while (storage.current.pos + shape[0].length > 10) {
                 storage.current.pos--
             }
+
             res.status(200).json(storage.current)
+
             break
         case "getId":
-            res.status(200).send(data.reduce((a,b)=>Math.max(a.gameId,b.gameId),0)+1)
+            let games=copy(data)
+            if(games.length==0){
+                res.status(200).send(1)
+            }
+            else if(games.length==1){
+                res.status(200).send(parseInt(games[0].gameId)+1)
+            }
+            else{
+                games=games.map((x)=>parseInt(x.gameId))
+                res.status(200).send(games.reduce((a,b)=>Math.max(a,b))+1)
+            }
+        case "reset":
+            storage=STORAGE_BASE
+            storage.gameId=gameId
+            res.status(200).send()
         default:
             res.status(404).send()
     }
     data[index]=storage
-    fs.writeFile("data/storage.json",JSON.stringify(data))
+    fs.writeFile("data/storage.json",JSON.stringify(data),(err)=>{
+        console.log(err)
+    })
 }
