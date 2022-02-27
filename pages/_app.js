96f8dbf1032df43626ee81ab83b8ee691bd5ba2a
BuildTools <ijamil.osoria@gmail.com>
2022-02-27 14:11:54 +0100

    imported new css

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import '../styles/main.css'
 import '../styles/normalize.css'
 import '../styles/docs.css'
+import '../styles/color-scheme.css'
 
 function MyApp({Component, pageProps}) {
     return <Component {...pageProps} />
